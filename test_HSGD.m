ab846cc300100959393ef52ead62b18ee631bf7a
HanQin Cai <44795174+caesarcai@users.noreply.github.com>
2023-08-24 23:25:03 -0400

    Update test_HSGD.m

--- test_HSGD.m
+++ test_HSGD.m
@@ -7,13 +7,13 @@ m = round(sample_rate*n);
 alpha = 0.1;              % 10% outlier
 k = round(alpha*m);
 
-[K,ox,~] = generate_signal(n,r,m,'true','false');
+[M,ox,~] = generate_signal(n,r,m,'true','false');
 
-ind = randsample(m,k);
+K = randsample(m,k);
 
-obs = ox(K);
+obs = ox(M);
 mean_of_ox = mean(abs(ox));
-obs(ind) = obs(ind) + 20*mean_of_ox*2*(rand(k,1)-0.5+1i*(rand(k,1)-0.5));
+obs(K) = obs(K) + 20*mean_of_ox*2*(rand(k,1)-0.5+1i*(rand(k,1)-0.5));
 
 eta = 0.6;
 gamma_init = max(min(1.5, (1-2*log(n)*r/n)*(1/alpha)),1.2);
@@ -21,7 +21,7 @@ gamma_decay = 0.95;
 tol = 1e-6;
 max_iter = 1000;
 proj = true;
-[x,err,timer] = HSGD(obs,n,r,K,alpha,eta,gamma_init,gamma_decay,proj,tol,max_iter);
+[x,err,timer] = HSGD(obs,n,r,M,alpha,eta,gamma_init,gamma_decay,proj,tol,max_iter);
 
 %SNR = snr(ox,ox-x)
 norm(ox-x)/norm(ox)
